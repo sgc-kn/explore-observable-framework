aa310cabd9bd6d0e7746c5cf5b8c000831665902
Patrik Keller <git@pkel.dev>
2024-07-03 08:03:18 +0200

    Add Einwohner dataset

--- src/data/pkel-test.csv.py
+++ src/data/pkel-test.csv.py
@@ -1,7 +1,7 @@
 import pandas as pd
 import sys
 
-df = pd.read_csv('data/merged_speed.csv')
+df = pd.read_csv('raw_data/merged_speed.csv')
 ss = df.query('index == 42')
 
 ss.to_csv(sys.stdout, index=False)
