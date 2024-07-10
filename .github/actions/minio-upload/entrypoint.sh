f3dd8db1682d1b0ed7c2e99ef0cc16d5e13abf5b
Patrik Keller <git@pkel.dev>
2024-07-10 12:11:34 +0200

    CI hacking

---
+++ .github/actions/minio-upload/entrypoint.sh
@@ -0,0 +1,12 @@
+#!/bin/sh -le
+
+cat << EOF | mc alias import alias
+{
+  "url": "${MINIO_ALIAS_URL}",
+  "accessKey": "${MINIO_ALIAS_ACCESS_KEY}",
+  "secretKey": "${MINIO_ALIAS_SECRET_KEY}",
+  "api": "${MINIO_ALIAS_API}"
+}
+EOF
+
+exec "$@"
